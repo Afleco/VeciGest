345e50a2840b93a956c9a4cacf0b9282b46d8f48
Alejandro <aflecodev@gmail.com>
2026-02-25 13:58:51 +0000

    Mejora responsive para dispositivos con botones inferiores

--- app/(Screens)/(admin)/GestionUsuarios.tsx
+++ app/(Screens)/(admin)/GestionUsuarios.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context'; // <-- NUEVO IMPORT
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Colors, FontSizes } from '../../../styles/theme';
 
 // Importar las pantallas de gestión
